9c10c2379bb9fd6ba6f1f6fa3fc534630875d947
sundaymba <sundinoh@gmail.com>
2024-01-23 06:19:49 +0000

    installed flask and other dep

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 # Installs puppet-lint
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
