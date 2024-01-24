72a8fc319eed9f5b9eef85e6eed2f04b678518f8
sundaymba <sundinoh@gmail.com>
2024-01-24 16:29:21 +0000

    update puppet before installing

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,41 +4,36 @@
 # curl in /
 # The redirection must be a “301 Moved Permanently”
 
+Exec { 'apt-update':
+command     => '/usr/bin/apt-get update',
+refreshonly => true,
+}
+
 # ensure nginx package resource is installed
 package {'nginx':
-ensure  => installed,
+ensure  => '1.18.0-0ubuntu1.4',
+require => Exec['apt-update'],
 }
 
 # create a file resource for index html
-file {'/etc/nginx/html/index.html':
-ensure  => file,
-content => "Hello World!",
+file {'/etc/nginx/index.html':
+ensure  => present,
+content => 'Hello World!',
 }
 
-# create a file for 404 error page
-file {'/etc/nginx/html/error_404.html':
-ensure   => file,
-content  => "Ceci n'est pas une page",
-}
 
 # defines the content of the config file in an embedded ruby file .erb
 file {'/etc/nginx/sites-enabled/default':
-ensure  => file,
+ensure  => present,
 content => "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /etc/nginx/html;
-	index index.html index.htm;
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /etc/nginx/;
+        index index.html index.htm;
 
-	location /redirect_me {
-		return 301 https://youtube.com/;
-	}
-
-	error_page 404 /error_404.html;
-	location /404 {
-		root /etc/nginx/html;
-		internal;
-	}
+        location /redirect_me {
+                return 301 https://youtube.com/;
+        }
     }",
 require => Package['nginx'],
 notify  => Service['nginx'],
