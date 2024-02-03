e981f234a2e4292d993c4c6da62b401ac1551d18
SundayMba <sundinoh@gmail.com>
2024-02-03 15:45:03 +0100

    added a custom header using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,40 +4,51 @@
 # curl in /
 # The redirection must be a “301 Moved Permanently”
 
-Exec { 'apt-update':
-command     => '/usr/bin/apt-get update',
-refreshonly => true,
-}
-
 # ensure nginx package resource is installed
 package {'nginx':
-ensure  => '1.18.0-0ubuntu1.4',
-require => Exec['apt-update'],
+ensure  => installed,
 }
 
 # create a file resource for index html
-file {'/var/www//html/index.html':
+file {'/var/www/html/index.html':
 ensure  => present,
 content => 'Hello World!',
 }
 
+# create a file for 404 error page
+file {'/var/www/html/error_404.html':
+ensure   => present,
+content  => "Ceci n'est pas une page",
+}
 
-# defines the content of the config file in an embedded ruby file .erb
-file {'/etc/nginx/sites-enabled/default':
+# create a default sites for nginx
+file {'/etc/nginx/sites-available/default':
 ensure  => present,
 content => "server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        index index.html index.htm;
-        add_header X-Served-By \$HOSTNAME;
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	add_header X-Served-By \$HOSTNAME
+	index index.html index.htm;
 
-        location /redirect_me {
-                return 301 https://youtube.com/;
-        }
+	location /redirect_me {
+		return 301 https://youtube.com/;
+	}
+
+	error_page 404 /error_404.html;
+	location /404 {
+		root /var/www/html;
+		internal;
+	}
     }",
 require => Package['nginx'],
 notify  => Service['nginx'],
+}
+
+file { '/etc/nginx/sites-enabled/default':
+ensure   => link,
+target   => '/etc/nginx/sites-available/default',
+notify   => Service['nginx'],
 }
 
 service {'nginx':
