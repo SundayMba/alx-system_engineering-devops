636a690e5f433149dd515c9821001dae798d1a71
SundayMba <sundinoh@gmail.com>
2024-03-06 09:27:54 +0100

    changed task 0 code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""
-Function to query subscribers in a given reddit subreddit
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Return total number of subs in a given subreddit """
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux/api.advance.0-sub/v1.0.0 (by sundaymba)"}
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404 or response.status_code == 302:
+    if response.status_code == 404:
         return 0
-    response = response.json()
-    results = response.get("data")
-    sub_count = results.get("subscribers")
-    return sub_count
+    results = response.json().get("data")
+    return results.get("subscribers")
